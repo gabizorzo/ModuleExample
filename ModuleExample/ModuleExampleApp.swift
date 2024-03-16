73703dd6ae4cc8979ad7fb100ed1132ec3d546db
Gabriela Zorzo <gabriela_zorzo@terceiros.sicredi.com>
2024-03-16 18:03:13 -0300

    start

---
+++ ModuleExample/ModuleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleExampleApp.swift
+//  ModuleExample
+//
+//  Created by Gabriela Zorzo on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ModuleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
